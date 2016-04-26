5e59f12a579bbfa5195a4b2298658dc55627b8c3
irfanash <irf2@paxym.com>
2016-04-26 15:17:51 -0700

    fix desired_device_state.json filename in parse script

--- runeio/generic/Packages-Dir/package-lib-rune/base-files/files/lib/rune/parse_desired_state.sh
+++ runeio/generic/Packages-Dir/package-lib-rune/base-files/files/lib/rune/parse_desired_state.sh
@@ -58,7 +58,7 @@ parse_desiredstatedoc_params () {
 }
 
 
-desiredstatedoc="/tmp/desired_state.curr"
+desiredstatedoc="/tmp/desired_device_state.json"
 if [ ! -e ${desiredstatedoc ]; then
 	echo "$0: Desired state doc: ${desiredstatedoc} not found"
 	exit 1
