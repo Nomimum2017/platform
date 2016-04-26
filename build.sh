2a8889a52797d08af0ff947d5b4ee2dd1860128b
irfanash <irf2@paxym.com>
2016-04-26 15:50:05 -0700

    package dest path mixup

--- build.sh
+++ build.sh
@@ -117,8 +117,8 @@ for i in `ls ../runeio/generic/Packages-Tarball/* 2> /dev/null`; do
 done
 
 # Add select directory packages
-check_place_directory ../runeio/generic/Packages-Dir/rune-lib package/base-files/files/etc/
-check_place_directory ../runeio/generic/Packages-Dir/rune-etc package/base-files/files/lib
+check_place_directory ../runeio/generic/Packages-Dir/rune-lib package/base-files/files/lib
+check_place_directory ../runeio/generic/Packages-Dir/rune-etc package/base-files/files/etc
 check_place_directory ../runeio/generic/Packages-Dir/rune-aws-iot-sdk package/utils/aws-iot/
 
 # Patch platform patches
